1895a3e2199654679a2012bf3e5ae1c51d40aae4
Tessa Thornton <tessathornton@gmail.com>
2014-06-05 21:54:22 -0400

    trello integration

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 (function(){
 
+  var userStory;
+
   var ui = {
     usButton: document.getElementById('submit'),
     nounInput: document.getElementById('noun'),
@@ -7,16 +9,27 @@
     output: document.getElementById('userstory'),
     outputContainer: document.getElementById('output'),
     inputContainer: document.getElementById('input'),
-    moreButton: document.getElementById('more')
+    moreButton: document.getElementById('more'),
+    trelloButton: document.getElementById('trello-launch'),
+    trelloContainer: document.getElementById('trello'),
+    closeTrello: document.getElementById('close-trello'),
+    back: document.getElementById('back')
   };
+
+  var templates = {
+    boards: document.getElementById('trello-boards'),
+    lists: document.getElementById('trello-lists'),
+    success: document.getElementById('card-success')
+  }
 
   function generateUserStory(e) {
     e.preventDefault();
     var noun = ui.nounInput.value || 'user';
     var action = ui.actionInput.value || 'order a widget';
-    var userStory = 'As a ' + noun + ', I want to ' + action + ',<br/> so that I can ' + action + ',<br/> because I\'d like to ' + action;
+    userStory = 'As a ' + noun + ', I want to ' + action + ',<br/> so that I can ' + action + ',<br/> because I\'d like to ' + action;
     ui.output.innerHTML = userStory;
     ui.outputContainer.classList.add('fade-in');
+    ui.inputContainer.classList.add('slide-down');
     return false;
   }
 
@@ -39,14 +52,89 @@
 
   function launchTrello(e) {
     e.preventDefault();
+    ui.trelloContainer.classList.add('pull-down');
     Trello.authorize({
       type: 'popup',
+      scope: {
+        read: true,
+        write: true,
+        account: true
+      },
       success: function() {
-        Trello.get("members/me/boards", { fields: "name" }, function(boards){
-          console.log(boards);
-        });
+        getBoards();
       }
     });
+  }
+
+  function getBoards() {
+    Trello.get("members/me/boards", { fields: "name" }, function(boards){
+      [].forEach.call(boards, function(board) {
+        renderBoard(board);
+      });
+    });
+  }
+
+  function renderBoard(board) {
+    var li = document.createElement('li');
+    li.innerHTML = board.name;
+    li.setAttribute('id', board.id);
+    li.addEventListener('click', selectBoard, false);
+    templates.boards.appendChild(li);
+  }
+
+  function selectBoard(e) {
+    templates.boards.classList.add('hidden');
+    templates.lists.innerHTML = '';
+    var id = e.target.getAttribute('id');
+    getLists(id);
+  }
+
+  function getLists(id) {
+    ui.back.classList.add('visible');
+    templates.lists.classList.remove('hidden');
+    Trello.get('boards/' + id + '/lists', function(lists){
+      [].forEach.call(lists, function(list){
+        renderList(list);
+      });
+    });
+  }
+
+  function renderList(list) {
+    var li = document.createElement('li');
+    li.innerHTML = list.name;
+    li.setAttribute('id', list.id);
+    li.addEventListener('click', selectList, false);
+    templates.lists.appendChild(li);
+  }
+
+  function selectList(e) {
+    var id = e.target.getAttribute('id');
+    var name = userStory.replace(/(<([^>]+)>)/ig,"");
+    Trello.post('lists/' + id + '/cards', {
+      name: name
+    }, function(card) {
+      postSuccess(card);
+    });
+  }
+
+  function postSuccess(card) {
+    templates.boards.classList.add('hidden');
+    templates.lists.classList.add('hidden');
+    templates.success.classList.add('visible');
+    var link = document.createElement('a');
+    link.innerHTML = card.name;
+    link.setAttribute('href', card.url);
+    templates.success.appendChild(link);
+  }
+
+  function closeTrello () {
+    ui.trelloContainer.classList.remove('pull-down');
+    ui.outputContainer.classList.add('slide-up');
+  }
+
+  function goBack() {
+    templates.lists.classList.add('hidden');
+    templates.boards.classList.remove('hidden');
   }
 
   ui.usButton.addEventListener('click', generateUserStory, false);
@@ -59,8 +147,10 @@
 
   ui.actionInput.addEventListener('focus', clearFocus, false);
 
-  var trello = document.getElementById('trello');
+  ui.trelloButton.addEventListener('click', launchTrello, false);
 
-  trello.addEventListener('click', launchTrello, false);
+  ui.closeTrello.addEventListener('click', closeTrello, false);
+
+  ui.back.addEventListener('click', goBack, false);
 
 })();
